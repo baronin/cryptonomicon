95e97853c1130b2f0c2012362926dd212b69915b
Yaroslav <baronin.yaroslav@gmail.com>
2022-09-01 20:31:20 +0300

    chore: move logic to computed and watch

--- src/App.vue
+++ src/App.vue
@@ -92,12 +92,12 @@
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in filteredTickers()"
+            v-for="t in paginatedTickers"
             :key="t.name"
             @click="select(t)"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
             :class="{
-              'border-4': sel === t
+              'border-4': selectedTicker === t
             }"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
@@ -133,20 +133,20 @@
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
 
-      <section v-if="sel" class="relative">
+      <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ sel.name }} - USD
+          {{ selectedTicker.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
-            v-for="bar in normalizeGraph()"
+            v-for="bar in normalizedGraph"
             :key="bar"
             :style="{ height: `${bar}%` }"
             class="bg-purple-800 border w-10"
           ></div>
         </div>
         <button
-          @click="sel = null"
+          @click="selectedTicker = null"
           type="button"
           class="absolute top-0 right-0"
         >
@@ -200,23 +200,30 @@ export default {
       filter: "",
       tickers: [],
       hasTicker: false,
-      sel: null,
+      selectedTicker: null,
       graph: [],
       topCoins: ["BTC", "DOGE", "BCH", "CHD"],
-      page: 1,
-      hasNextPage: true
+      page: 1
     };
   },
   created() {
     const windowData = Object.fromEntries(
       new URL(window.location).searchParams.entries()
     );
-    if (windowData.filter) {
+    /*  if (windowData.filter) {
       this.filter = windowData.filter;
     }
     if (windowData.page) {
       this.page = windowData.page;
-    }
+    } */
+    // оптимизация
+    const VALID_KEYS = ["filter", "page"];
+
+    VALID_KEYS.forEach((key) => {
+      if (windowData[key]) {
+        this[key] = windowData[key];
+      }
+    });
 
     const tickersData = localStorage.getItem("cryptonomicon-list");
     if (tickersData) {
@@ -234,20 +241,45 @@ export default {
     }
     document.addEventListener("click", this.handleClickOutside);
   },
-  methods: {
-    nextPage() {
-      this.page = this.page + 1;
+  computed: {
+    startIndex() {
+      return (this.page - 1) * 6;
+    },
+    endIndex() {
+      return this.page * 6;
     },
     filteredTickers() {
-      const start = (this.page - 1) * 6;
-      const end = this.page * 6;
-      const filteredTickers = this.tickers.filter((ticker) =>
-        ticker.name.includes(this.filter)
-      );
-
-      this.hasNextPage = filteredTickers.length > end;
-      return filteredTickers.slice(start, end);
+      return this.tickers.filter((ticker) => ticker.name.includes(this.filter));
     },
+    paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex);
+    },
+    hasNextPage() {
+      return this.filteredTickers.length > this.endIndex;
+    },
+    normalizedGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+
+      if (maxValue === minValue) return this.graph.map(() => 50);
+
+      // 5 + ((price - minValue) * 95) = 5 процентов у нас будет всегда
+      return this.graph.map(
+        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+    pageStateOptions() {
+      return {
+        filter: this.filter,
+        page: this.page
+      };
+    }
+  },
+  methods: {
+    nextPage() {
+      this.page = Number(this.page) + 1;
+    },
+
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -257,7 +289,7 @@ export default {
         this.tickers.find((t) => t.name === tickerName).price =
           data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecission(2);
 
-        if (this.sel?.name === tickerName) {
+        if (this.selectedTicker?.name === tickerName) {
           this.graph.push(data.USD);
         }
       }, 60000);
@@ -273,25 +305,23 @@ export default {
       // if (!this.ticker.length) return;
       const hasTicker = this.tickers.find((item) => item.name === this.ticker);
       if (hasTicker) return;
+
       const currentTicker = {
         name: this.ticker,
         price: "-"
       };
-      this.tickers.push(currentTicker);
-      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
-      this.subscribeToUpdates(currentTicker.name);
+
+      this.tickers = [...this.tickers, currentTicker];
       this.filter = "";
+
+      this.subscribeToUpdates(currentTicker.name);
     },
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter((item) => item !== tickerToRemove);
-    },
-    normalizeGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-      // 5 + ((price - minValue) * 95) = 5 процентов у нас будет всегда
-      return this.graph.map(
-        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
+
+      if (this.selectedTicker === tickerToRemove) {
+        this.selectedTicker = null;
+      }
     },
     onInputTickets() {
       this.hasTicker = false;
@@ -308,8 +338,7 @@ export default {
         .slice(0, 4);
     },
     select(ticker) {
-      this.sel = ticker;
-      this.graph = [];
+      this.selectedTicker = ticker;
     },
     onSelectCoin(e) {
       this.ticker = e;
@@ -323,27 +352,31 @@ export default {
     }
   },
   watch: {
+    selectedTicker() {
+      this.graph = [];
+    },
+    tickers(newValue, oldValue) {
+      // TODO почему не сработал watch при добавлении
+      console.log(newValue === oldValue);
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+    },
+    paginatedTickers() {
+      if (this.paginatedTickers.length === 0 && this.page > 1) {
+        // this.page = Math.max(1, this.page - 1);
+        this.page = Number(this.page) - 1;
+      }
+    },
     filter() {
       this.page = 1;
-      history.pushState(
-        null,
-        document.title,
-        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
-      );
     },
-    page() {
-      history.pushState(
+    pageStateOptions(value) {
+      window.history.pushState(
         null,
         document.title,
-        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+        `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
       );
     }
   },
-  // computed: {
-  //   checkHasTicker() {
-  //     return this.tickers.find((item) => item.name === this.ticker);
-  //   }
-  // },
   unmounted() {
     document.removeEventListener("click", this.handleClickOutside);
   }
